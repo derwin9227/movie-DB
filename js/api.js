f48cb07bbad999a1f50d2b03506ac007f0244153
derwin <derwin9227@gmail.com>
2023-02-17 00:55:23 -0500

    aplicando grid y media queries

--- js/api.js
+++ js/api.js
@@ -1,11 +1,21 @@
 //https://www.themoviedb.org/documentation/api
-//04495d415fb68e0f9f995482a0e44084
+//key= 04495d415fb68e0f9f995482a0e44084
+
+let templateMovie = (nombre,imagen) => `<div class="card">
+            <div class="card-info" style="background-image: url('https://image.tmdb.org/t/p/w500${imagen}')">
+
+                <p class="title">${nombre}</p>
+            </div>
+        </div>`;
+const container = document.querySelector(".container");
 
 const cargarpeliculas = () => {fetch("https://api.themoviedb.org/3/movie/popular?api_key=04495d415fb68e0f9f995482a0e44084")
     .then(res => res.json())
     .then(e => {
         e.results.forEach(element => {
-            console.log(`pelicula: ${element.title}`);
+
+            container.innerHTML += templateMovie(element.title, element.backdrop_path);
+            console.log(element);
         });
     })
     .catch(e => console.log(e));
