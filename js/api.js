b60cbe6afbd734da6e7be8f5f9ce9c39f3308ede
derwin <derwin9227@gmail.com>
2023-02-23 01:19:15 -0500

    validando paginacion

--- js/api.js
+++ js/api.js
@@ -5,11 +5,10 @@ const btnSiguiente = document.querySelector(".btnSiguiente");
 const btnAnterior = document.querySelector(".btnAnterior");
 const buscarPelicula = document.querySelector(".buscarPelicula");
 const inputBuscar = document.querySelector(".inputBuscar");
-let paginaActual =1, totalPaginas=0;
+let paginaActual=1, totalPaginas=0;
 
 let templateMovie = (nombre,imagen) => `<div class="card">
             <div class="card-info" style="background-image: url('https://image.tmdb.org/t/p/w500${imagen}')">
-
                 <p class="title">${nombre}</p>
             </div>
         </div>`;
@@ -20,7 +19,7 @@ const cargarpeliculas = (direccionPeliculas) => {fetch(direccionPeliculas)
     .then(e => {
         container.innerHTML = "";
         totalPaginas = e.total_pages;
-        console.log("total paginas"+totalPaginas);
+        console.log("total paginas "+totalPaginas);
         console.log("pagina actual "+paginaActual);
         e.results.forEach(element => {
             container.innerHTML += templateMovie(element.title, element.poster_path);
@@ -32,8 +31,25 @@ const cargarpeliculas = (direccionPeliculas) => {fetch(direccionPeliculas)
 
 cargarpeliculas(url);
 
-btnSiguiente.addEventListener("click", () => paginaActual === totalPaginas ? alert("No se puede avanzar mas") : cargarpeliculas(`${url}&page=${paginaActual++}`));
-btnAnterior.addEventListener("click", () => paginaActual===1 ? alert("No se puede retroceder mas") : cargarpeliculas(`${url}&page=${paginaActual--}`));
+btnSiguiente.addEventListener("click", () => {
+    if(paginaActual === totalPaginas){
+        alert("No se puede avanzar mas")
+    }
+    else{
+            paginaActual++;
+            cargarpeliculas(`${url}&page=${paginaActual}`);
+        }
+});
+
+btnAnterior.addEventListener("click", () => {
+    if(paginaActual===1)
+        alert("No se puede retroceder mas")
+    else{
+        paginaActual--;
+        cargarpeliculas(`${url}&page=${paginaActual}`)
+    }
+});
+
 buscarPelicula.addEventListener("click", () => {
     paginaActual=1;
     url=`https://api.themoviedb.org/3/search/movie?query=${inputBuscar.value}&api_key=04495d415fb68e0f9f995482a0e44084`;
